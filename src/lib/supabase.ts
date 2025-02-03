02b6a3740c6bfefaee9b8541069b98a663773e14
oshibusu <oshibu.matryo@gmail.com>
2025-02-03 15:12:42 -0800

    Delete supabase secret key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,7 +4,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const supabaseUrl = 'https://wqnbawipmolofzivwixt.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxbmJhd2lwbW9sb2Z6aXZ3aXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0NjM5MzUsImV4cCI6MjA1MTAzOTkzNX0.1h5Mzd6UEjc5PGyra1kdEu65NcXKLQ7VzEkv8DONInU';
-const supabaseRoleKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxbmJhd2lwbW9sb2Z6aXZ3aXh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNTQ2MzkzNSwiZXhwIjoyMDUxMDM5OTM1fQ.WZiMuXWTlEKS2kgKPfSvfjACA74aWoMHndaZLf9LmC0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
